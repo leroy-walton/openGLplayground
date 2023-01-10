e093f40d46e43b9f02f827313f98b5cac9e363e0
Leroy Walton <leroy.walton@protonmail.com>
2023-01-10 12:26:24 +0100

    test multi shader

--- engine/Main.cpp
+++ engine/Main.cpp
@@ -16,12 +16,11 @@
 #include <assimp/Importer.hpp>	// C++ importer interface
 #include <assimp/scene.h>		// Output data structure
 #include <assimp/postprocess.h> // Post processing flags
-#include <glm/gtx/string_cast.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
 #include "VAO.h"
 #include "EBO.h"
 #include "TexturedCube.h"
+#include <glm/gtx/string_cast.hpp>
 
 const unsigned int width = 2000;
 const unsigned int height = 1000;
@@ -68,45 +67,129 @@ void initOpenGl()
 	glDepthFunc(GL_LESS);
 }
 
+GLfloat vertices[] =
+{ //     COORDINATES     /        COLORS          /    TexCoord   /        NORMALS       //
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 5.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 5.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 0.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 0.0f,      0.8f, 0.5f,  0.0f, // Right side
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.8f, 0.5f,  0.0f, // Right side
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,      0.8f, 0.5f,  0.0f, // Right side
+
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, 0.5f,  0.8f, // Facing side
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,      0.0f, 0.5f,  0.8f, // Facing side
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,      0.0f, 0.5f,  0.8f  // Facing side
+};
+
+// Indices for vertices order
+GLuint indices[] =
+{
+	0, 1, 2, // Bottom side
+	0, 2, 3, // Bottom side
+	4, 6, 5, // Left side
+	7, 9, 8, // Non-facing side
+	10, 12, 11, // Right side
+	13, 15, 14 // Facing side
+};
+
+GLfloat lightVertices[] =
+{ //     COORDINATES     //
+	-0.1f, -0.1f,  0.1f,
+	-0.1f, -0.1f, -0.1f,
+	 0.1f, -0.1f, -0.1f,
+	 0.1f, -0.1f,  0.1f,
+	-0.1f,  0.1f,  0.1f,
+	-0.1f,  0.1f, -0.1f,
+	 0.1f,  0.1f, -0.1f,
+	 0.1f,  0.1f,  0.1f
+};
+
+GLuint lightIndices[] =
+{
+	0, 1, 2,
+	0, 2, 3,
+	0, 7, 4,
+	0, 3, 7,
+	3, 6, 7,
+	3, 2, 6,
+	2, 5, 6,
+	2, 1, 5,
+	1, 4, 5,
+	1, 0, 4,
+	4, 6, 5,
+	4, 7, 6
+};
+
 int main()
 {
 	GLFWwindow *window = initGlfwWindow();
 	initOpenGl();
 
-	Shader normalColorShader("normalColor.vert", "normalColor.frag");
-	Shader stupidShader("stupid.vert", "stupid.frag"); // compile shader
-	
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 3.0f)); // init camera
+	Shader shaderProgram("default.vert", "default.frag");
 
-	std::cout << "stupidShader.ID = " << stupidShader.ID << "\n";
-	std::cout << "normalColorShader.ID = " << normalColorShader.ID << "\n";
+	VAO VAO1;
+	VAO1.Bind();
+	// Generates Vertex Buffer Object and links it to vertices
+	VBO VBO1(vertices, sizeof(vertices));
+	// Generates Element Buffer Object and links it to indices
+	EBO EBO1(indices, sizeof(indices));
+	// Links VBO attributes such as coordinates and colors to VAO
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 11 * sizeof(float), (void*)0);                        // coords
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 11 * sizeof(float), (void*)(3 * sizeof(float)));		// color
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 11 * sizeof(float), (void*)(6 * sizeof(float)));		// texCoord
+	VAO1.LinkAttrib(VBO1, 3, 3, GL_FLOAT, 11 * sizeof(float), (void*)(8 * sizeof(float)));		// normals
+	// Unbind all to prevent accidentally modifying them
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
 
-	World world;
-	Model barrelModel("resources/models/wine_barrel/wine_barrel_01_4k.gltf");
-	Model sponzaModel("resources/models/sponza/Sponza.gltf");
-	Model suzanneModel("resources/models/suzanne/Suzanne2.gltf");
-	Model skullRatCubeModel("resources/models/cubeskullrat/cube_skull_rat.gltf");
-	
-	//WorldEntity sponza("sponza", &sponzaModel, &normalColorShader);
-	WorldEntity sponza("sponza", &sponzaModel, &stupidShader);
-	world.addEntity("sponza", &sponza);
-	WorldEntity skullRatCube("skullRatCube", &skullRatCubeModel, &stupidShader);
-	world.addEntity("skullRatCube", &skullRatCube);
-	WorldEntity suzanne("suzanne", &suzanneModel, &stupidShader);
-	world.addEntity("suzanne", &suzanne);
-	WorldEntity barrel("barrel", &barrelModel, &stupidShader);
-	world.addEntity("barrel", &barrel);
+	glm::vec4 lightColor = glm::vec4(1.0f, 0.8f, 1.0f, 1.0f);
+	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 lightModel = glm::mat4(1.0f);
+	lightModel = glm::translate(lightModel, lightPos);
 
-	WorldEntity test("test", &skullRatCubeModel, &stupidShader); // multiple entities can be linked to the same model.
-	world.addEntity("test", &test);
+	glm::vec3 pyramidPos = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::mat4 pyramidModel = glm::mat4(1.0f);
+	pyramidModel = glm::translate(pyramidModel, pyramidPos);
 
-	barrel.position = glm::vec3(50.0, -1.0f, 0.0f);
-	barrel.scaleUp(glm::vec3(50.0f ));
-	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 4.0f)); // init camera
+	shaderProgram.Activate();
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(pyramidModel));
+	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
 
-	// CubeMap cubeMap;
-	// Shader skyboxShader("skybox.vert", "skybox.frag");
-	// skyboxShader.Activate();
-	// glUniform1i(glGetUniformLocation(skyboxShader.ID, "skybox"), 0);
+	Texture tex(".", "resources/textures/3dfx-chip.png", aiTextureType_NONE);
+	GLint textureUniformLocation = glGetUniformLocation(shaderProgram.ID, "tex0");
+	glUniform1i(textureUniformLocation, 0); 
+	glBindTexture(GL_TEXTURE_2D, tex.id);	
+
+
+
+	Shader lightShader("light.vert","light.frag");
+	VAO lightVAO;
+	lightVAO.Bind();
+	VBO lightVBO(lightVertices, sizeof(lightVertices));
+	EBO lightEBO(lightIndices, sizeof(lightIndices));
+	lightVAO.LinkAttrib(lightVBO, 0, 3, GL_FLOAT, 3 * sizeof(float), (void*)0);
+	lightVAO.Unbind();
+	lightVBO.Unbind();
+	lightEBO.Unbind();
+
+	lightShader.Activate();
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
+	glUniform4f(glGetUniformLocation(lightShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+
+
 
 	FpsCounter fpsCounter;
 	GUI gui(window);
@@ -115,14 +198,8 @@ int main()
 	// Main loop
 	while (!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.12f, 0.16f, 0.21f, 1.0f );
+		glClearColor(0.45f, 0.03f, 0.44f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		// cubeMap.draw(skyboxShader, camera);
-		//stupidShader.Activate();
-		camera.Inputs(window);
-		camera.Matrix(normalColorShader, "proj");
-		camera.Matrix(stupidShader, "proj");
-
 		float crntTime = glfwGetTime();
 		if (crntTime - prevTime >= 1 / 60)
 		{
@@ -130,30 +207,21 @@ int main()
 			prevTime = crntTime;
 		}
 
-		glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-		glm::vec3 lightPos = glm::vec3(10.5f, 10.5f, 10.5f);
-		glm::mat4 lightModel = glm::mat4(1.0f);
+		camera.Inputs(window);
 
+		shaderProgram.Activate();
+		glUniform3f(glGetUniformLocation(shaderProgram.ID, "camPos"), camera.Position.x, camera.Position.y, camera.Position.z);
+		camera.Matrix(shaderProgram, "camMatrix");
+		tex.bind();
+		VAO1.Bind();
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
-		lightPos = glm::vec3(300.5f * sin(crntTime), 10.5f, cos(crntTime)*40.5f);
+		lightShader.Activate();
+		camera.Matrix(lightShader, "camMatrix");
+		lightVAO.Bind();
+		glDrawElements(GL_TRIANGLES, sizeof(lightIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
-		lightModel = glm::translate(lightModel, lightPos);
-		glUniform4f(glGetUniformLocation(stupidShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
-		glUniform3f(glGetUniformLocation(stupidShader.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
-		glUniform1f(glGetUniformLocation(stupidShader.ID, "time"), crntTime);
-
-		glUniform4f(glGetUniformLocation(normalColorShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
-		glUniform3f(glGetUniformLocation(normalColorShader.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
-		glUniform1f(glGetUniformLocation(normalColorShader.ID, "time"), crntTime);
-
-		skullRatCube.rotate(0.04f,  glm::vec3(0.0f, 1.0f, 0.0f));
-		suzanne.position=glm::vec3(sin(crntTime), 0.0f, cos(crntTime) )* 5.0f;
-		test.position=glm::vec3(-4.0f, 0.0f, 0.0f);
-		
-		world.draw();
-
-		gui.drawGUI(fpsCounter.getFps(), &world);
-		// gui.draw(fpsCounter.getFps(), world.getItems());
+	//	gui.drawGUI(fpsCounter.getFps());
 		glfwSwapBuffers(window);
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 			glfwSetWindowShouldClose(window, true);
